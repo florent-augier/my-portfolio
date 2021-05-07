74a763057a9b087633283c4d844476e0c54a7f09
florent <florent.sauvetage@gmail.com>
2021-05-07 15:37:42 +0200

    Feat(Footer): add content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="wrapperFooter">
       <div className="gridFooter">
-        <div className="firstCol">
+        <div className="gridItem">
           <div>
             Icons made by{" "}
             <a
@@ -20,8 +20,30 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        <div className="secondCol"></div>
-        <div className="thirdCol"></div>
+        <div className="gridItem">@ Florent Augier 2021</div>
+        <div className="gridItem">
+          Remerciements:
+          <ul>
+            <li>
+              {" "}
+              <a
+                href="https://www.lacapsule.academy/"
+                style={{ color: "var(--blue)" }}
+              >
+                @La Capsule
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.thehackingproject.org/"
+                style={{ color: "var(--blue)" }}
+              >
+                The Hacking Project
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
