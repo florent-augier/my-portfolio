49d97073268aea4ed670600db8eaefab643f72b8
florent <florent.sauvetage@gmail.com>
2021-07-16 15:21:02 +0200

    Feat(Projects): add project

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -5,6 +5,7 @@ import useWindowSize from "../helpers/WindowSize";
 
 import forky from "./../images/forky.png";
 import forkyPhone from "./../images/forky-phone.jpg";
+import leGold from "./../images/le-gold.png";
 
 import githubIcon from "../images/github.svg";
 
@@ -26,13 +27,16 @@ export default function Projects() {
 
   const forkyRef = useRef(null);
   const forkyPhoneRef = useRef(null);
+  const leGoldRef = useRef(null);
 
   const [forkyIsTouched, setForkyIsTouched] = useState(false);
   const [forkyPhoneIsTouched, setForkyPhoneIsTouched] = useState(false);
+  const [leGoldIsTouched, setLeGoldIsTouched] = useState(false);
 
   const [forkySwipeIsVisible, setForkySwipeIsVisible] = useState(false);
   const [forkyPhoneSwipeIsVisible, setForkyPhoneSwipeIsVisible] =
     useState(false);
+  const [leGoldSwipeIsVisible, setLeGoldSwipeIsVisible] = useState(false);
 
   const touchCard = (e, ref) => {
     if (
@@ -49,6 +53,13 @@ export default function Projects() {
     ) {
       setForkyPhoneIsTouched(!forkyPhoneIsTouched);
       setForkyPhoneSwipeIsVisible(!forkyPhoneSwipeIsVisible);
+    } else if (
+      e.type === "touchmove" &&
+      ref === leGoldRef &&
+      e.target.tagName === "IMG"
+    ) {
+      setLeGoldIsTouched(!leGoldIsTouched);
+      setLeGoldSwipeIsVisible(!leGoldSwipeIsVisible);
     }
   };
 
@@ -57,6 +68,8 @@ export default function Projects() {
       setForkyIsTouched(!forkyIsTouched);
     } else if (ref === forkyPhoneRef) {
       setForkyPhoneIsTouched(!forkyPhoneIsTouched);
+    } else if (ref === leGoldRef) {
+      setLeGoldIsTouched(!leGoldIsTouched);
     }
   };
 
@@ -75,6 +88,14 @@ export default function Projects() {
       forkyPhoneRef.current.style.transform = "rotateY(90deg)";
     }
   }, [forkyPhoneIsTouched]);
+
+  useEffect(() => {
+    if (leGoldIsTouched) {
+      leGoldRef.current.style.transform = "rotateY(0deg)";
+    } else {
+      leGoldRef.current.style.transform = "rotateY(90deg)";
+    }
+  }, [leGoldIsTouched]);
 
   return (
     <div className="bodyScreen">
@@ -168,6 +189,76 @@ export default function Projects() {
                     <p>React qui appelle le Backend</p>
                     <h3>Stratégie</h3>
                     <p>Application hybride: React encapsulée dans Express</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      {/* Mobilhome */}
+
+      <div className="sectionScreen">
+        <div className="headerSectionScreen">
+          <h2>Mobilhome Le Gold</h2>
+        </div>
+
+        {width <= 480 && (
+          <p style={{ fontSize: "12px", textAlign: "center" }}>
+            Toucher l'image pour accéder au lien ou swiper comme le montre
+            l'animation pour afficher les informations
+          </p>
+        )}
+
+        <a
+          href="https://mobilhome-le-gold.vercel.app"
+          onTouchMove={(e) => touchCard(e, leGoldRef)}
+          onMouseEnter={() => hoverCard(leGoldRef)}
+          onMouseLeave={() => hoverCard(leGoldRef)}
+        >
+          <div className="bodySectionScreen">
+            <div className="wrapperCard">
+              {width <= 480 && !forkySwipeIsVisible && (
+                <div
+                  className="swiper"
+                  style={{ position: "absolute", zIndex: "5" }}
+                >
+                  <lottie-player
+                    src="https://assets1.lottiefiles.com/datafiles/8rPPZlJdGjchPU2/data.json"
+                    style={{
+                      background: "transparent",
+                      float: "left",
+                      left: "50%",
+                      maxWidth: width >= 480 ? "300px" : "150px",
+                      maxHeight: width >= 480 ? "120px" : "60px",
+                    }}
+                    speed="1"
+                    loop
+                    autoplay
+                  ></lottie-player>
+                </div>
+              )}
+
+              <div className="card">
+                <div className="image">
+                  <img src={leGold} alt="forky"></img>
+                </div>
+
+                <div className="details" ref={leGoldRef}>
+                  <div className="details-text">
+                    <h3>C'est quoi ? </h3>
+
+                    <p>
+                      Site web statique de présentation d'un mobilhome situé à
+                      Crots and les Hautes-Alpes.
+                    </p>
+                    <h3>Backend: </h3>
+                    <p>Pas de Back</p>
+                    <h3>Frontend: </h3>
+                    <p>NextJs</p>
+                    <h3>Stratégie</h3>
+                    <p>Aucune</p>
                   </div>
                 </div>
               </div>
